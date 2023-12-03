069595ff0b241245d748a8e832a8c10c22c9f86c
iris-col <71783366+iris-col@users.noreply.github.com>
2023-12-04 01:36:02 +0900

    Add files via upload

---
+++ question/js/data_en.js
@@ -0,0 +1,120 @@
+const qnaList = [
+    {
+      q: '1. What kind of atmosphere makes you the most comfortable?',
+      a: [
+        { answer: 'a. exciting and energetic', type: ['1'] },
+        { answer: 'b. calm and calm', type: ['0','3'] },
+        { answer: 'c. romantic and sweet', type: ['2' ] },
+      ]
+    },
+    {
+      q: '2. What is your favorite instrument? ',
+      a: [
+        { answer: 'a. Guitar', type: ['0','1','2', '3'] },
+        { answer: 'b. Piano', type: ['2','0'] },
+        { answer: 'c. String instruments (violins, cellos, etc.)', type: ['0'] },
+      ]
+    },
+    {
+      q: '3. Which theme of the lyrics touches your heart more?',
+      a: [
+        { answer: 'a. love and separation', type: ['2', '0'] },
+        { answer: 'b. nature and scenery', type: ['1', '3'] },
+        { answer: 'c. the meaning and philosophy of life', type: ['0', '3'] }
+      ]
+    },
+    {
+      q: '4. What kind of music genre do you prefer??',
+      a: [
+        { answer: 'a. pop/rock" ', type: ['0', '1' ] },
+        { answer: 'b. classic/jazz" ', type: ['0', '3' ] },
+        { answer: 'c. Hip-hop/R&B" ', type: ['2', '0', '3' ] },
+      ]
+    },
+    {
+      q: '5. What are the characteristics of the music that you are impressed with?',
+      a: [
+        { answer: 'a. harmony of melody and rhythm', type: ['0', '3' ] },
+        { answer: 'b. a vocal that expresses emotions', type: ['2', '1', '0', '3']},
+        { answer: 'c. complex instrumental performance', type: ['2'] },
+      ]
+    },
+  
+    {
+      q: '6. When do you listen to music the most? ',
+      a: [
+        { answer: 'a. Using public transportation', type: ['0', '1', '2','3' ] },
+        { answer: 'b. When I was working', type: ['2'] },
+        { answer: 'c. When I rest', type: ['0', '1' ] },
+      ]
+    },
+    {
+      q: '7. What kind of music will make your daily life more enjoyable?',
+      a: [
+        { answer: 'a.  a lively pop song', type: ['1' ] },
+        { answer: 'b. a mysterious electronic', type: ['3' ] },
+        { answer: 'c. emotional indie music', type: ['0', '2', '3' ] },
+      ]
+    },
+    {
+      q: '8. What rhythm will make your heart beat more?',
+      a: [
+        { answer: 'a. a fast tempo beat', type: ['1' ] },
+        { answer: 'b. a mid-tempo groove', type: ['2', '1', '0', '3' ] },
+        { answer: 'c. a comfortable rhythm with a slow tempo', type: ['0', '3' ] },
+      ]
+    },
+    {
+      q: '9. Do you focus on the musical part rather than the lyrics, or are you sensitive to the meaning of the lyrics?',
+      a: [
+        { answer: 'a. The rhythm and melody of the music are more important!', type: ['0', '3' ] },
+        { answer: 'b. The emotion and story of the lyrics are important to me!', type: ['2', '0', '3' ] },
+        { answer: 'c. It is good to balance the music with the lyrics!', type: ['0', '3' ] },
+      ]
+    },
+    {
+      q: '10. Which music best suits your sensibility? ',
+      a: [
+        { answer: 'a. dreamy and elegant music', type: ['2', '0', '3'] },
+        { answer: 'b. Powerful and energetic music', type: ['1'] },
+        { answer: 'c. profound and emotional music', type: ['0', '3' ] },
+      ]
+    },
+    {
+      q: '11. In what environment do you prefer to listen to music?',
+      a: [
+        { answer: 'a. in nature, looking at the scenery', type: ['0', '1'] },
+        { answer: 'b. Walking on the street or using public transportation, in the midst of the disturbance of the city', type: ['1', '3'] },
+        { answer: 'c.In a comfortable space at home', type: ['2' ] },
+      ]
+    },
+    {
+      q: '12. Lastly, what is the purpose of listening to music??',
+      a: [
+        { answer: 'a. To get pleasure and joy', type: ['1' ] },
+        { answer: 'b. To express or empathize with your feelings', type: ['2', '1', '0', '3'] },
+        { answer: 'c. To explore and discover new music', type: ['0', '3'] },
+      ]
+    }
+  ]
+  
+  const infoList = [
+    {
+      name: 'It is a song that makes you think with the meaning of the lyrics and the beautiful voice,  &ltALEPH\>',
+      desc: ' There are a lot of lyrics written by the singer for the ALEPH song, so it is sure to convey the emotions Why do not you listen to it when you need to think? '
+    },
+    {
+      name: 'It is a song style where 4 sweet people meet to create a fantastic harmony , &ltLUCY\>',
+      desc: 'Why do not you listen to it when you want to harmonize with the change of emotions and the time you think about it because it has the characteristics of bringing out empathy and reflection by capturing your experiences and emotions in the lyrics? '
+    },
+    {
+      name: 'It is a special song that conveys emotions with lyrics that have a sweet voice and deep emotions ,  &ltSong Hae Yeah\>',
+      desc: 'Why do not you listen to it when you want to harmonize with the change of emotions and the time you think about it because it has the characteristics of bringing out empathy and reflection by capturing your experiences and emotions in the lyrics?'
+    },
+    {
+      name: 'He has a versatile singing voice and a variety of musical styles , &ltLee Jun Hyeong\>',
+      desc: 'We focus on delivering touching emotions, and we give depth to the expression of emotions with lyrics and melodies. Why do not you listen to it when you want to be touched or comforted?'
+    },
+    
+  ]
+  
